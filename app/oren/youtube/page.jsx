e5398a4dca1160589c9611971dabfa6e5d7cca65
Oren <78069187+oomari@users.noreply.github.com>
2024-03-04 23:28:59 -0500

    oren side bar

--- app/oren/youtube/page.jsx
+++ app/oren/youtube/page.jsx
@@ -2,138 +2,15 @@ import React from "react";
 import NavBar from "../../../components/NavBar.jsx";
 import SideBar from "../../../components/SideBar.jsx";
 
-const Tags = [
-  "All",
-  "New York Jets",
-  "Mixes",
-  "Music",
-  "Live",
-  "Miranda Sings",
-  "Gaming",
-  "Podcasts",
-  "New York Jets",
-  "Mixes",
-  "Music",
-  "Live",
-  "Miranda Sings",
-  "Gaming",
-  "Podcasts",
-];
 
-const Videos = [
-  {
-    id: 1,
-    title: "shady fiances caught CHEATING on tiktok - REACTION",
-    channel: {
-      name: "Charlottle Dobre",
-      photoUrl:
-        "https://yt3.ggpht.com/bgT46DBGl-pR73SwTVsDoZn1NCPmttrOezRW1EfQv9LRBU-apJtnIn0HyrdFEPnJqwfxTAoU=s68-c-k-c0x00ffffff-no-rj",
-    },
-    thumbnailUrl:
-      "https://i.ytimg.com/vi/7wz6zk6ZyTE/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAhkuJpz46SoK6IoMW5ngUCzdY6Gw",
-    views: 824000,
-    createdAt: "",
-  },
-  {
-    id: 1,
-    title: "shady fiances caught CHEATING on tiktok - REACTION",
-    channel: {
-      name: "Charlottle Dobre",
-      photoUrl:
-        "https://yt3.ggpht.com/bgT46DBGl-pR73SwTVsDoZn1NCPmttrOezRW1EfQv9LRBU-apJtnIn0HyrdFEPnJqwfxTAoU=s68-c-k-c0x00ffffff-no-rj",
-    },
-    thumbnailUrl:
-      "https://i.ytimg.com/vi/7wz6zk6ZyTE/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAhkuJpz46SoK6IoMW5ngUCzdY6Gw",
-    views: 824000,
-    createdAt: "",
-  },
-  {
-    id: 1,
-    title: "shady fiances caught CHEATING on tiktok - REACTION",
-    channel: {
-      name: "Charlottle Dobre",
-      photoUrl:
-        "https://yt3.ggpht.com/bgT46DBGl-pR73SwTVsDoZn1NCPmttrOezRW1EfQv9LRBU-apJtnIn0HyrdFEPnJqwfxTAoU=s68-c-k-c0x00ffffff-no-rj",
-    },
-    thumbnailUrl:
-      "https://i.ytimg.com/vi/7wz6zk6ZyTE/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAhkuJpz46SoK6IoMW5ngUCzdY6Gw",
-    views: 824000,
-    createdAt: "",
-  },
-  {
-    id: 1,
-    title: "shady fiances caught CHEATING on tiktok - REACTION",
-    channel: {
-      name: "Charlottle Dobre",
-      photoUrl:
-        "https://yt3.ggpht.com/bgT46DBGl-pR73SwTVsDoZn1NCPmttrOezRW1EfQv9LRBU-apJtnIn0HyrdFEPnJqwfxTAoU=s68-c-k-c0x00ffffff-no-rj",
-    },
-    thumbnailUrl:
-      "https://i.ytimg.com/vi/7wz6zk6ZyTE/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAhkuJpz46SoK6IoMW5ngUCzdY6Gw",
-    views: 824000,
-    createdAt: "",
-  },
-  {
-    id: 1,
-    title: "shady fiances caught CHEATING on tiktok - REACTION",
-    channel: {
-      name: "Charlottle Dobre",
-      photoUrl:
-        "https://yt3.ggpht.com/bgT46DBGl-pR73SwTVsDoZn1NCPmttrOezRW1EfQv9LRBU-apJtnIn0HyrdFEPnJqwfxTAoU=s68-c-k-c0x00ffffff-no-rj",
-    },
-    thumbnailUrl:
-      "https://i.ytimg.com/vi/7wz6zk6ZyTE/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAhkuJpz46SoK6IoMW5ngUCzdY6Gw",
-    views: 824000,
-    createdAt: "",
-  },
-  {
-    id: 1,
-    title: "shady fiances caught CHEATING on tiktok - REACTION",
-    channel: {
-      name: "Charlottle Dobre",
-      photoUrl:
-        "https://yt3.ggpht.com/bgT46DBGl-pR73SwTVsDoZn1NCPmttrOezRW1EfQv9LRBU-apJtnIn0HyrdFEPnJqwfxTAoU=s68-c-k-c0x00ffffff-no-rj",
-    },
-    thumbnailUrl:
-      "https://i.ytimg.com/vi/7wz6zk6ZyTE/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAhkuJpz46SoK6IoMW5ngUCzdY6Gw",
-    views: 824000,
-    createdAt: "",
-  },
-];
 
 export default function Home() {
   return (
     <main>
       <NavBar />
       <SideBar />
-      <section className="absolute left-48 right-0 top-14 bottom-0 px-8 overflow-y-auto">
-        <div className="flex gap-x-2 overflow-x-auto py-3 no-scrollbar">
-          {Tags.map((tag) => (
-            <button className="bg-gray-100 px-4 py-2 text-sm rounded-xl font-medium whitespace-nowrap scroll">
-              {tag}
-            </button>
-          ))}
-        </div>
-        <div className="bg-white grid grid-cols-3 gap-x-6 gap-y-10 py-2">
-          {Videos.map((video) => (
-            <div data-name="video-card" className="flex flex-col gap-y-4">
-              <img src={video.thumbnailUrl} className="rounded-xl" />
-              <div data-name="video-card-details" className="flex gap-x-3">
-                <div className="w-[12.5%]">
-                  <img src={video.channel.photoUrl} className="rounded-full" />
-                </div>
-                <div>
-                  {" "}
-                  <h1 className="font-medium mb-2">{video.title}</h1>
-                  <h3 className="text-sm text-gray-500">
-                    {video.channel.name} <br />
-                    {video.views} views • 5 months ago
-                  </h3>
-                </div>
-              </div>
-            </div>
-          ))}
-        </div>
+      <section className="bg-green-500 absolute left-48 top-14 right-0 bottom-0 overflow-y-auto p-8">
+        <div className="h-[10000px] bg-white">Helloooooooooo</div>
       </section>
     </main>
   );
--- components/SideBar.jsx
+++ components/SideBar.jsx
@@ -16,7 +16,83 @@ export default function SideBar() {
           <HomeIcon className="w-1/5 size-6"/>
           Home
         </button>
+        <button className="flex px-3 py-2 items-center gap-x-5 rounded-xl text-sm hover:bg-gray-100">
+          <SiYoutubeshorts className="w-1/5 size-6"/>
+          Shorts
+        </button>
+        <button className="flex px-3 py-2 items-center gap-x-5 rounded-xl text-sm hover:bg-gray-100">
+          <MdOutlineSubscriptions className="w-1/5 size-6"/>
+          Subscriptions
+        </button>
       </div>
+      <hr className="bg-gray-100"/>
+      <div className="flex flex-col p-2">
+        <h1 className="mx-4 flex items-center">
+          You
+          <IoChevronForwardSharp className="mx-2 size-4"/>
+        </h1>
+        <button className="flex px-3 py-2 items-center gap-x-5 rounded-xl text-sm hover:bg-gray-100">
+          <UserCircleIcon className="w-1/5 size-6"/>
+          Your channel
+        </button>
+        <button className="flex px-3 py-2 items-center gap-x-5 rounded-xl text-sm hover:bg-gray-100">
+          <GoHistory className="w-1/5 size-6"/>
+          History
+        </button>
+        <button className="flex px-3 py-2 items-center gap-x-5 rounded-xl text-sm hover:bg-gray-100">
+          <AiOutlinePlaySquare className="w-1/5 size-6"/>
+          Your videos
+        </button>
+        <button className="flex px-3 py-2 items-center gap-x-5 rounded-xl text-sm hover:bg-gray-100">
+          <LuClock4 className="w-1/5 size-6"/>
+          Watch later
+        </button>
+        <button className="flex px-3 py-2 items-center gap-x-5 rounded-xl text-sm hover:bg-gray-100">
+          <SlArrowDown className="w-1/5 size-3"/>
+          Show more
+        </button>
+      </div>
+      <hr className="bg-gray-100"/>
+      <div className="flex flex-col p-2">
+        <h1 className="mx-4 flex items-center font-bold text-sm">
+          Subscriptions
+        </h1>
+        <button className="flex px-3 py-2 items-center gap-x-5 rounded-xl text-sm hover:bg-gray-100">
+          <img 
+          src="https://yt3.ggpht.com/tMCgP0E_2g9p_B70OuxeR4gBj_HNQkTlHXvlqLqWMBhoBWIbzsPhtIAle3ljauTFmaMZstWeHw=s88-c-k-c0x00ffffff-no-rj" 
+          className="w-1/5 rounded-full"/>
+          Matt O'Leary
+        </button>
+        <button className="flex px-3 py-2 items-center gap-x-5 rounded-xl text-sm hover:bg-gray-100">
+          <GoHistory className="w-1/5 size-6"/>
+          Jets Talk 24/7
+        </button>
+        <button className="flex px-3 py-2 items-center gap-x-5 rounded-xl text-sm hover:bg-gray-100">
+          <AiOutlinePlaySquare className="w-1/5 size-6"/>
+          RealBreakingNate
+        </button>
+        <button className="flex px-3 py-2 items-center gap-x-5 rounded-xl text-sm hover:bg-gray-100">
+          <LuClock4 className="w-1/5 size-6"/>
+          Just One Cookbook
+        </button>
+        <button className="flex px-3 py-2 items-center gap-x-5 rounded-xl text-sm hover:bg-gray-100">
+          <LuClock4 className="w-1/5 size-6"/>
+          PowerfulJRE
+        </button>
+        <button className="flex px-3 py-2 items-center gap-x-5 rounded-xl text-sm hover:bg-gray-100">
+          <LuClock4 className="w-1/5 size-6"/>
+          Bro Code
+        </button>
+        <button className="flex px-3 py-2 items-center gap-x-5 rounded-xl text-sm hover:bg-gray-100">
+          <LuClock4 className="w-1/5 size-6"/>
+          GREENBEANJETSFAN 
+        </button>
+        <button className="flex px-3 py-2 items-center gap-x-5 rounded-xl text-sm hover:bg-gray-100">
+          <SlArrowDown className="w-1/5 size-3"/>
+          Show 12 more
+        </button>
+      </div>
+      <hr className="bg-gray-100"/>
     </aside>
   );
 }
