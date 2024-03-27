a9364bd21655ddb25a20b976c6768530a62336b7
Oren <78069187+oomari@users.noreply.github.com>
2024-03-27 10:54:11 -0400

    the first part of how to implement useParams

--- app/oren/youtube/watch/[id]/page.jsx
+++ app/oren/youtube/watch/[id]/page.jsx
@@ -1,8 +1,22 @@
 "use client";
 import NavBar from "@/components/NavBar";
+import { useParams } from "next/navigation";
 import React from "react";
+import { useState, useEffect } from "react";
 
 export default function Watch() {
+  const [video, setVideo] = useState([]);
+  const { id } = useParams();
+
+  useEffect(() => {
+    (async () => {
+      const resp = await fetch(`/api/videos/${id}`);
+      const json = await resp.json();
+      const _video = json.video;
+      setVideo(_video);
+    })();
+  }, []);
+
   return (
     <main>
       <NavBar />
@@ -11,6 +25,20 @@ export default function Watch() {
           <section className="bg-blue-100 w-2/3">
             <div className="ml-6 pt-6 pr-6">
               <div className="w-full aspect-video bg-gray-500"></div>
+              <h1 className="text-xl font-bold">
+                Talkin Jets Draft - OL if Bowers is Picked!
+              </h1>
+              <div className="flex">
+                <div className="flex flex-col ">
+                  <h1>Talkin Jets</h1>
+                  <h3 className="text-xs ">5.77K...</h3>
+                </div>
+                <div className="mx-6">
+                  <button className="bg-gray-300 border-1 rounded-xl w-10 h-8">
+                    Join
+                  </button>
+                </div>
+              </div>
               <h3>Title</h3>
               <h3>Title</h3>
               <h3>Title</h3>
